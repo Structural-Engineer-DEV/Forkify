6334a6b07fdaece2d610b7f04ca50dbcc8ee3e9c
Structural-Engineer-DEV <mbkrystek@gmail.com>
2023-03-11 15:09:39 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f048b367-8b7e-41ca-bee3-31a6d845becc';
+export const MODAL_CLOSE_SEC = 2.5;
